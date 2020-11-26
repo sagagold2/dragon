908e4bc95df55831a5e8aaa79fad433e13b1dfe9
heedonlim <63532258+heedonlim@users.noreply.github.com>
2020-11-26 11:29:47 +0900

    skybox change

--- Assets/Scriptes/BackGroundScrolling.cs
+++ Assets/Scriptes/BackGroundScrolling.cs
@@ -1,26 +1,25 @@
 ﻿using UnityEngine;
+using System;
 
 public class BackGroundScrolling : MonoBehaviour
 {
-    public float speed = 10f;
-
-    public GameObject[] backGrounds;
-
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
+      
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.back * speed * Time.deltaTime);
+     transform.Translate(Vector3.back * speed * Time.deltaTime);
         Debug.Log("맵 움직인다");
-        if (transform.localPosition.z < 200f)
+        if (transform.localPosition.z < -200f)
         {
-            transform.localPosition = new Vector3(0, 0, 400);
+            transform.localPosition = new Vector3(0, 0, 300);
             Debug.Log("맵 위치 바뀐다");
         }
     }
